4a1f24f950782e1d7f1a220fc8136d0d2d646c2e
Shubham Nath <shubhamnath95@gmail.com>
2023-03-31 03:02:24 +0000

    Git setup singapore

---
+++ git-singapore.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+yum install git -y
+yum update -y
+git --version
+git config --global user.name "Shubham Nath"
+git config --global user.email "shubhamnath95@gmail.com"
+git config --list
+ls
