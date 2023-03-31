0a1ebdf0022758bb3cc73da24bbcd84e28a90ede
Subhasish Mishra <subhasishmishra2@gmail.com>
2023-03-31 02:32:28 +0000

    Script for git srt up in mumbai

---
+++ git.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+yum install git -y
+yum update -y
+which git
+git --version
+git config --global user.name "Subhasish Mishra"
+git config --global user.email "subhasishmishra2@gmail.com"
+git config --list
+ls
+
